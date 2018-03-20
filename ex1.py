1ecff650d8ed93bf7ae86b720d2a77552502b19e
teztripathy <34486360+teztripathy@users.noreply.github.com>
2018-03-20 19:10:48 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 
-x = int(input("Give a number:  "))
-i = 1
-s1 = ""
-while (i <= x):
-    s1 += str(i % 10)
-    i += 1
-print(s1)
+
+def ruler():
+    x = int(input("Give a number:  "))
+    i = 1
+    s1 = ""
+    s2 = " "
+    while (i <= x):
+        s1 += str(i % 10)
+        i += 1
+        if (i % 10 == 0):
+            s2 += str(int(i / 10))
+        else:
+            s2 += " "
+    print(s2)
+    print(s1)
+
+
+ruler()
